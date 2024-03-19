9b25daeb51306b72516ffffe4f59c3498fec9df1
Elijah Matar <m.matar515@gmail.com>
2024-03-19 16:59:17 -0500

    Clean up code

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -48,12 +48,14 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   // update a category by its `id` value
   try{
-    const categoryData = await Category.update({
-      where: {
-        id: req.params.id
-      }
-    })
-    res.status(200).json(categoryData);
+    const categoryData = await Category.update(
+      { category_name: req.body.category_name },
+      { where: { id: req.params.id } }
+    );
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with this id'});
+      return;
+    };
   } catch (err) {
     res.status(500).json(err)
   }
@@ -63,14 +65,13 @@ router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
   try{
     const categoryData = await Category.destroy({
-      where: {
-        id: req.params.id
-      }
+      where: { id: req.params.id }
     });
     if (!categoryData) {
       res.status(404).json({ message: 'No category found with this id'});
       return;
     };
+    res.status(200).json(categoryData)
   } catch (err) {
     res.status(500).json(err)
   }
