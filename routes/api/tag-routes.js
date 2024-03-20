00e3afae85f7157d7e542315a7e20b3e1917079b
Elijah Matar <m.matar515@gmail.com>
2024-03-20 12:34:34 -0500

    Included correct responses and comments on delete request

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -8,11 +8,14 @@ router.get('/', async (req, res) => {
   // find all tags
   try {
     const tagData = await Tag.findAll({
-// include associated Product data
-      include: [{ model: Product }]
-    })
+    // include associated Product data
+      include: [{ model: Product, through: ProductTag, as: 'products'} ]
+    });
+    // return tags with associated products
     res.status(200).json(tagData);
+      // catch error
   } catch (err) {
+    console.log(err);
     res.status(500).json(err);
   }
 });
@@ -22,7 +25,7 @@ router.get('/:id', async (req, res) => {
   try {
     const tagData = await Tag.findByPk(req.params.id, {
 // include its associated Product data
-      include: [{ model: Product }]
+      include: [{ model: Product, through: ProductTag, as: 'products'} ]
     })
     if (!tagData) {
       res.status(404).json({ message: 'No tag found with this id'})
@@ -47,10 +50,9 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
   try {
-    const tagData = await Tag.update(req.params.id, {
-      where: {
-        id: req.params.id
-      }
+    const tagData = await Tag.update(
+      { tag_name: req.body.tag_name },
+      { where: { id: req.params.id }
     });
     if (!tagData) {
       res.status(404).json({ message: 'No tag found with this id'})
@@ -65,7 +67,7 @@ router.put('/:id', async (req, res) => {
 router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
   try {
-    const tagData = await Tag.destroy(req.params.id, {
+    const tagData = await Tag.destroy({
       where: {
         id: req.params.id
       }
@@ -74,6 +76,7 @@ router.delete('/:id', async (req, res) => {
       res.status(404).json({ message: 'No tag found with this id'})
       return;
     }
+    res.status(204).end();
   } catch (err) {
     res.status(500).json(err);
   }
